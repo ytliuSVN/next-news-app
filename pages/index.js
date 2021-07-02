847436cba04e2b39da08f9553cad5daeeb7c78d9
penny.liu <penny.liu@kaiostech.com>
2021-07-02 23:02:04 +0800

    :sparkles: Fine-tune Layout Component

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,31 @@ import React from 'react';
 import Head from 'next/head';
 import styles from '../styles/Home.module.scss';
 
-function Home() {
+function Home({ users }) {
   return (
-    <div className={styles.container}>
+    <div className='container'>
       <Head>
         <title>KaiOS | News</title>
       </Head>
 
-      <main className={styles.main}>
-        <h1>Home page</h1>
+      <main>
+        <h1>Top Stories</h1>
+        <section>111</section>
+
+        <h1>Sports</h1>
+        <section>222</section>
       </main>
     </div>
   );
 }
+
+// export const getStaticProps = async () => {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/todos/');
+//   const data = await res.json();
+
+//   return {
+//     props: { users: data },
+//   };
+// };
 
 export default Home;
