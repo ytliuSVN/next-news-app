4f9d014907b92c7dad1c683f6304b5d968a9dcbf
penny.liu <penny.liu@kaiostech.com>
2021-07-05 10:49:38 +0800

    :sparkles: NProgress bar set up

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,23 @@
 import '../styles/globals.scss';
 import Layout from '../components/Layout/Layout';
 
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+// NProgress Customization
+NProgress.configure({
+  minimum: 0.3,
+  easing: 'ease',
+  speed: 800,
+  showSpinner: false,
+});
+
+// Show a loading bar when changing routes
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
+
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
