aeb276dbc3ed8be0232874f7623352dff92e11ea
penny.liu <penny.liu@kaiostech.com>
2021-07-08 23:03:09 +0800

    :construction: Implement search functionality

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,23 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import axios from 'axios';
 import styles from './SearchBar.module.scss';
 
 function SearchBar() {
-  const [allData, setAllData] = useState([]);
-  const [filteredData, setFilteredData] = useState(allData);
+  const clickHandler = () => {
+    console.log('Click');
+  };
 
-  const handleSearch = (event) => {};
+  const onKeyDownHandler = () => {
+    console.log('KeyDown');
+  };
 
   return (
-    <div className={styles.search}>
-      <div className={styles.searchInputs}>
-        <input type='text' placeholder='Search all news' />
-        <div className={styles.searchBtn}>
+    <form onKeyDown={onKeyDownHandler}>
+      <div className={styles.search}>
+        <div className={styles.searchInputs}>
+          <input type='text' placeholder='Search all news' />
+        </div>
+        <div className={styles.searchBtn} onClick={clickHandler}>
           <div className={styles.searchIcon}></div>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
 
