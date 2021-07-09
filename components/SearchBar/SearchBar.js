b94d3477be5817a40c6d44fa4551a7c0ca51e466
penny.liu <penny.liu@kaiostech.com>
2021-07-09 23:52:29 +0800

    :construction: Implement search functionality

--- components/SearchBar/SearchBar.js
+++ components/SearchBar/SearchBar.js
@@ -1,25 +1,53 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import getConfig from 'next/config';
 import styles from './SearchBar.module.scss';
 
 function SearchBar() {
-  const clickHandler = () => {
-    console.log('Click');
+  const { publicRuntimeConfig } = getConfig();
+
+  const [page, setPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleChange = (e) => {
+    setSearchTerm(e.target.value);
   };
 
-  const onKeyDownHandler = () => {
-    console.log('KeyDown');
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   axios(
+  //     `${publicRuntimeConfig.GUARDIAN_API_URL}search?page=${page}&page-size=15&q=${searchTerm}&show-fields=body,headline,thumbnail&api-key=${publicRuntimeConfig.GUARDIAN_API_KEY}`
+  //   )
+  //     .then((response) => {
+  //       console.log(response.data);
+  //     })
+  //     .catch((error) => {
+  //       setIsLoading(false);
+  //       console.log('Error getting news data: ' + error);
+  //     })
+  //     .finally(function () {
+  //       setIsLoading(false);
+  //     });
+  // }, [searchTerm]);
+
+  const clickHandler = () => {
+    // expanded with animation
   };
 
   return (
-    <form onKeyDown={onKeyDownHandler}>
-      <div className={styles.search}>
-        <div className={styles.searchInputs}>
-          <input type='text' placeholder='Search all news' />
-        </div>
-        <div className={styles.searchBtn} onClick={clickHandler}>
-          <div className={styles.searchIcon}></div>
-        </div>
+    <div className={styles.search}>
+      <div className={styles.searchInputs}>
+        <input
+          type='text'
+          placeholder='Search all news'
+          onChange={handleChange}
+        />
       </div>
-    </form>
+      <div className={styles.searchBtn} onClick={clickHandler}>
+        <div className={styles.searchIcon}></div>
+      </div>
+    </div>
   );
 }
 
